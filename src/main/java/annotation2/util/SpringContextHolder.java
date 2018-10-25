19b2ede0ca2ecb52bc893439c3db0c27ef34b24d
gityejing <mailfendou@163.com>
2018-10-25 11:46:18 +0800

    aware 相关

--- src/main/java/annotation2/util/SpringContextHolder.java
+++ src/main/java/annotation2/util/SpringContextHolder.java
@@ -1,24 +1,28 @@
 package annotation2.util;
 
 import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringValueResolver;
 
 /**
- * 以静态变量保存Spring ApplicationContext, 
- * 可在任何代码任何地方任何时候取出ApplicaitonContext.
+ * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候取出ApplicaitonContext.
+ * 
  * @author 30000133
  *
  */
 @Component
 @Lazy(false)
-public class SpringContextHolder implements ApplicationContextAware,DisposableBean  {
-	
+public class SpringContextHolder
+		implements ApplicationContextAware, DisposableBean, BeanNameAware, EmbeddedValueResolverAware {
+
 	private static ApplicationContext applicationContext = null;
- 
+
 	/**
 	 * 获取静态变量中的ApplicationContext.
 	 */
@@ -26,7 +30,7 @@ public class SpringContextHolder implements ApplicationContextAware,DisposableBe
 		assertContextInjected();
 		return applicationContext;
 	}
- 
+
 	/**
 	 * 从静态变量applicationContext中得到Bean, 自动转型为所赋值对象的类型.
 	 */
@@ -35,7 +39,7 @@ public class SpringContextHolder implements ApplicationContextAware,DisposableBe
 		assertContextInjected();
 		return (T) applicationContext.getBean(name);
 	}
- 
+
 	/**
 	 * 从静态变量applicationContext中得到Bean, 自动转型为所赋值对象的类型.
 	 */
@@ -43,14 +47,14 @@ public class SpringContextHolder implements ApplicationContextAware,DisposableBe
 		assertContextInjected();
 		return applicationContext.getBean(requiredType);
 	}
- 
+
 	/**
 	 * 清除SpringContextHolder中的ApplicationContext为Null.
 	 */
 	public static void clearHolder() {
 		applicationContext = null;
 	}
- 
+
 	/**
 	 * 实现ApplicationContextAware接口, 注入Context到静态变量中.
 	 */
@@ -58,7 +62,7 @@ public class SpringContextHolder implements ApplicationContextAware,DisposableBe
 	public void setApplicationContext(ApplicationContext applicationContext) {
 		SpringContextHolder.applicationContext = applicationContext;
 	}
- 
+
 	/**
 	 * 检查ApplicationContext不为空.
 	 */
@@ -71,5 +75,16 @@ public class SpringContextHolder implements ApplicationContextAware,DisposableBe
 	public void destroy() throws Exception {
 		SpringContextHolder.clearHolder();
 	}
- 
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println(name);
+	}
+
+	// 解析字符串的值
+	@Override
+	public void setEmbeddedValueResolver(StringValueResolver resolver) {
+		resolver.resolveStringValue("${hello}");	
+	}
+
 }
